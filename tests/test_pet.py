e210870033ccae219318a57454b9b012e3f579dc
Sergey Kiyanitsyn <k.sergey@wcode.club>
2026-07-11 15:55:18 +0300

    add test "get_nonexistent_pet"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -33,3 +33,13 @@ class TestPet:
         with (allure.step("Проверка текстового содержания ответа")):
             assert response.text == "Pet not found", f"Ожидался текст 'Pet not found', а пришел {response.text}"
 
+    @allure.title("Попытка получить данные несуществующего питомца")
+    def test_get_nonexistent_pet(self):
+        with allure.step("Получение запроса несуществующего питомца"):
+            response = requests.get(url=f"{BASE_URL}/pet/9999")
+
+        with allure.step("Проверка статус ответа"):
+            assert response.status_code == 404, f"Ожидался статус кода 404, а пришел {response.status_code}"
+
+        with (allure.step("Проверка текстового содержания ответа")):
+            assert response.text == "Pet not found", f"Ожидался текст 'Pet not found', а пришел {response.text}"
